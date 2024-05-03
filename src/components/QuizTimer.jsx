66806e2ddb6f08946f39a5e04a401699ee4a3014
PierreBarge <pierrebarge@hotmail.fr>
2024-05-03 15:03:27 +0200

    Feature: The timer hourglass got a little animation when time's going

--- src/components/QuizTimer.jsx
+++ src/components/QuizTimer.jsx
@@ -1,10 +1,21 @@
 import PropTypes from "prop-types";
 
-function QuizTimer({ timeRemaining }) {
+function QuizTimer({ timeRemaining, questionTiming }) {
+  const hourglassAnimation = () => {
+    if (questionTiming === "Ongoing") {
+      return { animation: "3s ease-in-out 0s infinite rotation" };
+    }
+  };
+
   return (
     <div id="quiz-timer">
       <p id="timer-text">{timeRemaining}</p>
-      <img id="hourglass" src="src/assets/hourglass.svg" width="35px" />
+      <img
+        id="hourglass"
+        src="src/assets/hourglass.svg"
+        width="35px"
+        style={hourglassAnimation()}
+      />
     </div>
   );
 }
@@ -13,4 +24,5 @@ export default QuizTimer;
 
 QuizTimer.propTypes = {
   timeRemaining: PropTypes.number.isRequired,
+  questionTiming: PropTypes.string.isRequired,
 };
