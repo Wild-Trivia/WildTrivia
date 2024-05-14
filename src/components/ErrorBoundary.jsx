f1c0d7d37e291f39989b79796775f241ae361867
PierreBarge <pierrebarge@hotmail.fr>
2024-05-14 15:46:23 +0200

    Feature: Added Error Boundary to quiz page

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, info.componentStack);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  fallback: PropTypes.any,
+  children: PropTypes.any,
+};
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import { useProfile } from "../contexts/useProfile";
+
+export default function ErrorPage() {
+  const { profile } = useProfile();
+
+  const errorPageStyle = () => {
+    switch (profile.theme) {
+      case "Classic":
+        return { backgroundImage: "none" };
+      case "Starry Sky":
+        return { backgroundImage: "url('/src/assets/starry-sky.jpg')" };
+      case "Night Jungle":
+        return { backgroundImage: "url('/src/assets/night-jungle.jpg')" };
+    }
+  };
+
+  const colorStyle = () => {
+    switch (profile.theme) {
+      case "Classic":
+        return { color: "black" };
+      case "Starry Sky":
+      case "Night Jungle":
+        return { color: "white" };
+    }
+  };
+
+  const errorQuitButtonStyle = () => {
+    switch (profile.theme) {
+      case "Classic":
+        return { color: "#7b59da", border: "1px solid #7b59da" };
+      case "Starry Sky":
+        return { color: "#00688b", border: "1px solid #00688b" };
+      case "Night Jungle":
+        return { color: "#044c7f", border: "1px solid #044c7f" };
+    }
+  };
+
+  return (
+    <div id="error-page-container" style={errorPageStyle()}>
+      <p id="error-text" style={colorStyle()}>
+        Sorry, something went wrong!
+      </p>
+      <img id="error-image" src="/src/assets/error.png" width="100%" />
+      <p id="error-description" style={colorStyle()}>
+        The API might be down (you can check the API state{" "}
+        <a href="https://stats.uptimerobot.com/r99niw4gk">with this link</a>
+        ), or maybe you just tried to create a custom quiz with a specific
+        theme/difficulty combination for which the API couldn&apos;t supply
+        enough entries...
+      </p>
+      <div id="error-quit-container">
+        <Link to="/" id="error-quit-button" style={errorQuitButtonStyle()}>
+          Back to Home Page
+        </Link>
+      </div>
+    </div>
+  );
+}
